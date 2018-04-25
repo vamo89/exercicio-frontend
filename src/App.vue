bddc91f356cc8bf5aa092f36bf67ee3a8a77ea76
Victor Oliveira <vamo89@gmail.com>
2018-04-24 21:23:22 -0300

    Configuring Vue.js

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <header>
+      <profilePic />
+      <info>
+        <name/>
+        <title/>
+        <social>
+          <ul>
+            <li>twitter</li>
+            <li>google+</li>
+            <li>linkedIn</li>
+            <li>github</li>
+            <li>yahoo?</li>
+          </ul>
+        </social>
+      </info>
+      <contact-btn/>
+    </header>
+    <main>
+      <about-me/>
+      <projets>
+        Latest Projects
+        <ul>
+          <li repeat="projects">
+            <project>
+              <img />
+              <title />
+              <description />
+              <link />
+            </project>
+
+            <project featured>
+              <title />
+              <subtitle />
+              <img big />
+              <description />
+              <link button />
+            </project>
+          </li>
+        </ul>
+      </projets>
+      <info/>
+      <skills/>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>
