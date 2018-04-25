60185b4ca6a979fe603b051f203fcb36be50cf34
Victor Oliveira <vamo89@gmail.com>
2018-04-24 22:47:08 -0300

    Adding vue-resource to use .get

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueResource from 'vue-resource'
 
-import './assets/css/reset.css'
+import App from './App.vue'
+import '../static/css/reset.css'
+
+Vue.use(VueResource)
 
 new Vue({
   el: '#app',
