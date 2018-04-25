29c872b9842e95b0b8b856a9c77d9e2ad95165f7
Victor Oliveira <vamo89@gmail.com>
2018-04-25 19:58:51 -0300

    Adding vue-awesome to use icons

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+import Icon from 'vue-awesome/components/Icon'
 
 import App from './App.vue'
 import '../static/css/reset.css'
 
+// Importing all icons
+// TODO: change that to import only the ones in use
+import 'vue-awesome/icons'
+
+Vue.component('icon', Icon)
 Vue.use(VueResource)
 
 new Vue({
